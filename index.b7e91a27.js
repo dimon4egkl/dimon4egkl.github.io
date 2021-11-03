44f8aae89e3d282ab5a2e1069ad60eb8c212aca1
Dmytro Klymiuk <dimon4egkl55@gmail.com>
2021-11-03 10:48:15 +0200

    Updates

---
+++ index.b7e91a27.js
@@ -0,0 +1,2 @@
+!function(){const e=document.querySelector("#like");e.addEventListener("click",(()=>e.style.color="red"))}(),function(){const e=document.querySelector("footer");let n=(new Date).toLocaleDateString("en-US",{weekday:"long",year:"numeric",month:"long",day:"numeric"});e.innerHTML=n}();
+//# sourceMappingURL=index.b7e91a27.js.map
