8f26dd0c03174093670ce211edd6db1bfb4a1e1f
Dmytro Klymiuk <dimon4egkl55@gmail.com>
2021-11-03 19:04:49 +0200

    Updates

---
+++ index.434f166d.js
@@ -0,0 +1,2 @@
+!function(){const e=document.querySelector("#like");e.addEventListener("click",(()=>e.style.color="red"))}(),function(){const e=document.querySelector("footer");let t=(new Date).getFullYear();e.innerHTML+=t}();
+//# sourceMappingURL=index.434f166d.js.map
